a81f1e93cd55879a65d8ae340adf968cdd37a674
NAJIM PINJARI <143899549+najimpinjari@users.noreply.github.com>
2023-10-30 18:15:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // CONDITIONAL STATMENT //
+
+            int A = 11;
+            if (A == 10) {
+                Console.WriteLine($" A IS TEN ");
+            }
+            else {
+                Console.WriteLine($" A IS NOT TEN "); 
+                    }
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
