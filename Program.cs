96e873ba3bc1872de4c1177c3fe0b428c4efc4d5
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-15 10:10:34 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,138 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace Array_types
+namespace _17_ClassObjectDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-                       
-                          int i = 10 ;
-                        int j ;
-                          int 10;
-                        //Array - declare array
+            int i = 0; // i is a variable of type int
 
-                        // declare array first way 
+            // Student s1; // s1 is a variable of type Student
+            // s1 is a reference variable
 
-                        int[] fees = new int[5];
-                        fees[0] = 12000;
-                        fees[1] = 13000;
-                        fees[2] = 14000;
-                        fees[3] = 15000;
-                        fees[4] = 16000;
+            Student s1 = new Student(); // object or instance of Student class
+            s1.RollNumber = 1;
+            s1.Name = "Ajay";
+            s1.City = "Pune";
+            // s1.CourseName = "Dot Net";
+            s1.Print();
 
-                        Console.WriteLine($"number of atems ={fees.Length}");
+            Student s2 = new Student();
+            s2.Create(2, "Chetan", "Mumbai");
+            // s2.CourseName = "Dot Net";
+            s2.Print();
 
-                        // declare array second way
+            Student.CourseName = "C#"; // we can change value of static field
 
-                        string[] names = new string[5]
-                        {"sagar","karan","gopal","radhesh","gotya"};
-                        Console.WriteLine($" \n number of names :  {names.Length}");
+            Student s3 = new Student();
+            s3.Create(3, "Farin", "Pune");
+            s3.Print();
 
-                        for (int i = 0; i < names.Length; i++)
-                        {
-                            Console.WriteLine($" {names[i]} "); 
-                        }
+            //World w1 = new World(); // error creating object of static class
 
-                        string email = null;
-                      Console.WriteLine($"number of email : {email.Length}"); // runtime error 
-           
-                        // reverce array 
+            //World w2 = new World(); // error creating object of static class
 
-                                    int[] numbers = new int[] { 9,8,7,6,5,4,3,2,1 };
-                                    Console.WriteLine("original items");
-                                    for (int i = 0; i < numbers.Length; i++)
-                                    { 
-                                        Console.Write($"{numbers[i]} ");
-                                    }
-                                    Console.WriteLine($"\n Reverce Array");
-                                    for (int i = numbers.Length - 1; 1 >= 0; i--)
-                                    {
-                                        Console.Write($"{numbers[i]}");  
-
-
-                        int[] numbers = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
-                        int highest = 0;
-                        for (int i = 0;i < numbers.Length; i++) 
-                        { 
-                            for (int j = i  + 1 ; j < numbers.Length; j++)
-                            {
-                                if (numbers[j] > highest) 
-                                {
-                                    highest = numbers[j];
-                                }
-                            }
-                        }
-                        Console.WriteLine($"\n highest number : {highest}");
-
-                        // reverce Array    o/p 1,2,3,4,5,6,7,8,9,
-
-                        Console.WriteLine("origin items");
-                        for (int i = 0; i < names.Length ; i++)
-                        {
-                            Console.Write($"{numbers[i]}");
-                        }
-                        Console.WriteLine("\nreverce array");
-                        for ( int i =  numbers.Length - 1; i >= 0 ; i--)
-                        {
-                            Console.Write($"{numbers[i]}");
-                        }
-           
-
-            //find higher number 
-            // int[] numbers = new int[] { 10, 23, 45, 34, 67, 76, 234, 576 };
-
-            int[] numbers = new int[] { 10, 23, 45, 34, 67, 76, 234, 576 };
-
-              int highest = 0;
-              for (int i = 0; i < numbers.Length; i++)
-              {
-                  //highest = numners [i];
-                  for (int j = i; j  < numbers.Length; j++)
-                  {
-                      if (numbers[j] > highest)
-                      {
-                          highest = numbers[j];
-                      }
-                  }
-              }
-                  Console.WriteLine($"\n highest number : {highest}");
- 
-
-            // reversce inpir string 
-
-           Console.WriteLine("please enter your name");
-             string name = Console.ReadLine();
-
-             string reverceName = string.Empty;
-             for (int i = name.Length - 1 ; i >= 0; i--)
-             {
-                 reverceName += name[i];
-             }
-             Console.WriteLine($" inpur : {name} output : {reverceName}" );
-
-
-
- 
-
-            // reverce number in sentence 
-
-            Console.WriteLine("please enter your sentence ");
-            string sentence = Console.ReadLine();
-
-            string[] world = sentence.Split(new char[] { ' ' });
-
-            Console.WriteLine("output  : \n ");
-
-            for (int i = world.Length - 1; i >= 0; i--)
-            {
-                Console.Write($"{world[i]}");
-            }
+            Console.WriteLine($"{World.NumberOfCountries}");
 
             Console.ReadLine();
         }
     }
+
+    class Student
+    {
+        public int RollNumber; // instance member
+        public string Name;
+        public string City;
+        public static string CourseName = "Dot Net"; // static member
+
+        public void Print() // non static/ instance
+        {
+            Console.WriteLine($"Roll Number : {this.RollNumber} Name : {this.Name}" +
+                $" City : {this.City} Course Name : {CourseName}");
+
+            // PrintA();
+        }
+
+        public void Create(int RollNumber, string Name, string City)
+        {
+            this.RollNumber = RollNumber;
+            this.Name = Name;
+            this.City = City;
+        }
+
+        public static void PrintA() // static method
+        {
+            Console.WriteLine("static PrintA() called");
+            //Console.WriteLine($"Roll Number : {RollNumber} Name : {Name}" +
+            //    $" City : {City} Course Name : {CourseName}");
+
+            // Print(); // not allowed to call static method directly
+
+            // to access instance member from static method we need to create object
+            //Student s = new Student();
+            //s.Create(4, "Kishor", "Pune");
+        }
+    }
+
+    static class World
+    {
+        public static int NumberOfCountries = 290;
+
+        public static int NumberOfContinents = 7;
+    }
 }
+
